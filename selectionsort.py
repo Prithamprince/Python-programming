7bbca4abddc272330ce0a0a11b8b871e69e721ec
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-09-20 20:09:39 +0530

    Create selectionsort.py

---
+++ selectionsort.py
@@ -0,0 +1,10 @@
+array=list(map(int,input().split(" ")))
+size=len(array)
+for i in range(0,size):
+   for j in range(i+1,size):
+        if(array[j]<array[i]):
+              min=array[j]
+              array[j]=array[i]
+              array[i]=min
+print(*array,sep=" ")
+       
